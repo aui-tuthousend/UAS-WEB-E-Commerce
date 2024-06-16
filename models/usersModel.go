62244e8e118bbe4099dd70b5cba753d6da9dc38c
aui_tuthousend <mraihan286@gmail.com>
2024-06-16 10:00:26 +0700

    feat: user model & controller

---
+++ models/usersModel.go
@@ -0,0 +1,13 @@
+package models
+
+import (
+	"gorm.io/gorm"
+)
+
+type User struct {
+	gorm.Model
+	Username string  `json:"username" gorm:"unique"`
+	Email    string  `json:"email" gorm:"unique"`
+	Password string  `json:"-"`
+	Saldo    float64 `gorm:"default:0"`
+}
