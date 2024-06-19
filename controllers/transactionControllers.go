d5a6f8713be71d2cb132b77a46927699d62c6d9e
aui_tuthousend <mraihan286@gmail.com>
2024-06-20 05:19:44 +0700

    feat: transaction

--- controllers/transactionControllers.go
+++ controllers/transactionControllers.go
@@ -1,39 +1,91 @@
 package controllers
 
 import (
-	"fmt"
 	"github.com/gofiber/fiber/v2"
-	"net/http"
+	"log"
 	"strconv"
 	"web_uas/initializers"
 	"web_uas/models"
 )
 
+type CheckoutPayload struct {
+	SelectedItems []string `json:"selectedItems"`
+}
+
 func Checkout(c *fiber.Ctx) error {
-	selectedItems := c.FormValue("selectedItems")
-	//userID := c.Params("userID").(uint) // Assuming userID is set in context, adjust as necessary
+	var payload CheckoutPayload
 
-	if selectedItems == "" {
-		return c.Status(http.StatusBadRequest).SendString("No items selected for checkout")
+	if err := c.BodyParser(&payload); err != nil {
+		log.Println("Error parsing JSON:", err)
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "Cannot parse JSON",
+		})
 	}
 
-	itemIDs := []string{selectedItems}
-	var selectedProducts []models.DetailWishlist
-	for _, itemID := range itemIDs {
-		id, err := strconv.Atoi(itemID)
+	total := 0
+
+	newTransaction := models.Transaction{
+		IdUser: 1,
+	}
+
+	if err := initializers.GetDB().Create(&newTransaction).Error; err != nil {
+		return c.Status(fiber.StatusInternalServerError).SendString("Failed to create transaction")
+	}
+
+	for _, wislis := range payload.SelectedItems {
+		id, err := strconv.ParseUint(wislis, 10, 32)
 		if err != nil {
-			return c.Status(http.StatusBadRequest).SendString("Invalid item ID")
+			return c.Status(fiber.StatusBadRequest).SendString("Invalid product ID")
 		}
 
-		var item models.DetailWishlist
-		if err := initializers.GetDB().First(&item, id).Error; err != nil {
-			return c.Status(http.StatusInternalServerError).SendString("Failed to retrieve item")
+		var DWislis models.DetailWishlist
+		if err := initializers.GetDB().First(&DWislis, uint(id)).Error; err != nil {
+			return c.Status(fiber.StatusNotFound).SendString("Product not found")
 		}
 
-		selectedProducts = append(selectedProducts, item)
+		var productReal models.Product
+		if err := initializers.GetDB().First(&productReal, DWislis.IdProduct).Error; err != nil {
+			return c.Status(fiber.StatusNotFound).SendString("Product not found")
+		}
 
-		fmt.Println(item.ProductName)
+		temptot := DWislis.Quantity * DWislis.ProductPrice
+		total += temptot
+
+		newProductCopies := models.ProductCopy{
+			ProductName:        productReal.ProductName,
+			ProductDescription: productReal.ProductDescription,
+			ProductImageCover:  productReal.ProductImageCover,
+			Quantity:           DWislis.Quantity,
+			ProductPrice:       productReal.ProductPrice,
+		}
+
+		if err := initializers.GetDB().Create(&newProductCopies).Error; err != nil {
+			return err
+		}
+
+		newDT := models.DetailTransaction{
+			IdTransaction:   newTransaction.ID,
+			IdProductCopies: newProductCopies.ID,
+			Quantity:        DWislis.Quantity,
+			Total:           temptot,
+		}
+
+		if err := initializers.GetDB().Create(&newDT).Error; err != nil {
+			return err
+		}
+
+		if err := initializers.GetDB().Delete(&models.DetailWishlist{}, DWislis.ID).Error; err != nil {
+			return c.Status(fiber.StatusInternalServerError).SendString("Failed to delete product")
+		}
 	}
 
-	return c.Redirect("/")
+	newTransaction.TotalPrice = total
+
+	if err := initializers.GetDB().Save(&newTransaction).Error; err != nil {
+		return err
+	}
+
+	return c.JSON(fiber.Map{
+		"message": "Checkout successful",
+	})
 }
