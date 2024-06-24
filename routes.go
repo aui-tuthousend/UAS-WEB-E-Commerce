2b8519432703a0724504e04874ece74872bdbd36
aui_tuthousend <mraihan286@gmail.com>
2024-06-24 17:56:46 +0700

    update: routes

--- routes.go
+++ routes.go
@@ -10,12 +10,13 @@ func SetupRoutes(app *fiber.App) {
 	app.Post("/insertWishlist/:idUser/:idProduct", controllers.InsertIntoWishlist)
 	app.Post("/storeProduct", controllers.StoreProduct)
 	app.Post("/checkout", controllers.Checkout)
+	app.Post("/updateWishlistQuantity/:idDQ", controllers.UpdateWishlistQ)
 
 	app.Get("/createProduct", func(c *fiber.Ctx) error { return c.Render("produk/createProduct", fiber.Map{}) })
 	app.Get("/", controllers.ShowProduct)
 	app.Get("/produk/:id", controllers.ViewProduct)
 
 	app.Get("/wishlist/:idUser", controllers.ShowWishList)
-	app.Post("/updateWishlistQuantity/:idDQ", controllers.UpdateWishlistQ)
+	app.Get("/history/:idUser", controllers.ShowTransaction)
 
 }
