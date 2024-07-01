49fd400472562e533528184ea57bbc39aa8c5506
aui_tuthousend <mraihan286@gmail.com>
2024-07-01 09:04:47 +0700

    feat: routes & middleware

--- routes.go
+++ routes.go
@@ -7,16 +7,43 @@ import (
 
 func SetupRoutes(app *fiber.App) {
 
-	app.Post("/insertWishlist/:idUser/:idProduct", controllers.InsertIntoWishlist)
-	app.Post("/storeProduct", controllers.StoreProduct)
-	app.Post("/checkout", controllers.Checkout)
-	app.Post("/updateWishlistQuantity/:idDQ", controllers.UpdateWishlistQ)
+	m := func(c *fiber.Ctx) error {
+		store := controllers.Store
 
-	app.Get("/createProduct", func(c *fiber.Ctx) error { return c.Render("produk/createProduct", fiber.Map{}) })
-	app.Get("/", controllers.ShowProduct)
-	app.Get("/produk/:id", controllers.ViewProduct)
+		sess, err := store.Get(c)
+		if err != nil {
+			return c.Status(fiber.StatusInternalServerError).SendString("Internal Server Error")
+		}
 
-	app.Get("/wishlist/:idUser", controllers.ShowWishList)
-	app.Get("/history/:idUser", controllers.ShowTransaction)
+		idU := sess.Get("IDuser")
+		if idU == nil {
+			return c.Redirect("/login")
+		}
+
+		return c.Next()
+	}
+
+	//auth := app.Group("/", m)
+
+	app.Get("/regis", func(c *fiber.Ctx) error { return c.Render("sesi/regis", fiber.Map{}) })
+	app.Post("/register", controllers.Register)
+
+	app.Get("/login", func(c *fiber.Ctx) error { return c.Render("sesi/login", fiber.Map{}) })
+	app.Post("/loginVerif", controllers.Login)
+	app.Get("/", func(c *fiber.Ctx) error { return c.Redirect("/login") })
+
+	app.Get("/logout", m, controllers.LogOut)
+
+	app.Post("/insertWishlist/:idUser/:idProduct", m, controllers.InsertIntoWishlist)
+	app.Post("/checkout", m, controllers.Checkout)
+	app.Post("/updateWishlistQuantity/:idDQ", m, controllers.UpdateWishlistQ)
+	app.Post("/storeProduct", m, controllers.StoreProduct)
+
+	app.Get("/createProduct", m, func(c *fiber.Ctx) error { return c.Render("produk/createProduct", fiber.Map{}) })
+	app.Get("/home", m, controllers.ShowProduct)
+	app.Get("/produk/:id", m, controllers.ViewProduct)
+
+	app.Get("/wishlist/:idUser", m, controllers.ShowWishList)
+	app.Get("/history/:idUser", m, controllers.ShowTransaction)
 
 }
